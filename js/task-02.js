7676d5a674039449ac388faec1bede5c037f4035
TatyanaZdota <99191661+TatyanaZdota@users.noreply.github.com>
2022-10-10 21:55:02 +0300

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,4 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const 
+const ingredientsEl = document.querySelector('#ingredients')
+const itemEl = ingredients.map(ingredient => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.classList.add('item');
+  ingredientEl.textContent=ingredient;
+  ingredientsEl.append(ingredientEl);
+  return
+}); 
+
+console.log (ingredientsEl)
