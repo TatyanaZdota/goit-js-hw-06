88f5f6e7301520e55965f0b57d2259945d9a480a
TatyanaZdota <99191661+TatyanaZdota@users.noreply.github.com>
2023-11-06 19:41:04 +0200

    add refs

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,12 @@
-const inputEl = document.querySelector('#validation-input');
+const inputEl = document.querySelector("#validation-input");
 
+inputEl.addEventListener("blur", onInputElBlur);
 
-inputEl.addEventListener('blur', onInputElBlur);
-
-function onInputElBlur (event) {
-event.currentTarget.value.length === parseInt(inputEl.dataset.length) ? 
-inputEl.classList.add('valid') || inputEl.classList.replace('invalid', 'valid'): inputEl.classList.add('invalid') || inputEl.classList.replace('valid', 'invalid');
-};
-
+function onInputElBlur(event) {
+  event.currentTarget.value.length ===
+  parseInt(event.currentTarget.dataset.length)
+    ? event.currentTarget.classList.add("valid") ||
+      event.currentTarget.classList.replace("invalid", "valid")
+    : event.currentTarget.classList.add("invalid") ||
+      event.currentTarget.classList.replace("valid", "invalid");
+}
--- js/task-10.js
+++ js/task-10.js
@@ -1,31 +1,33 @@
-const buttonCreate = document.querySelector('[data-create]');
-const buttonDestroy = document.querySelector('[data-destroy]');
-const inputValueEl = document.querySelector("input")
-const boxesEl = document.querySelector('#boxes')
+const refs = {
+  buttonCreate: document.querySelector("[data-create]"),
+  buttonDestroy: document.querySelector("[data-destroy]"),
+  inputValue: document.querySelector("input"),
+  boxes: document.querySelector("#boxes"),
+};
 
-buttonCreate.addEventListener('click', onButtonCreateClick);
-buttonDestroy.addEventListener('click', onButtonDestroyClick);
+refs.buttonCreate.addEventListener("click", onButtonCreateClick);
+refs.buttonDestroy.addEventListener("click", onButtonDestroyClick);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}; 
+}
 
-function onButtonCreateClick (event) {createBoxes(inputValueEl.value)};
-function onButtonDestroyClick (event) {boxesEl.innerHTML = ''};
-  
+function onButtonCreateClick(event) {
+  createBoxes(refs.inputValue.value);
+}
+function onButtonDestroyClick(event) {
+  refs.boxes.innerHTML = "";
+}
+
 function createBoxes(amount) {
-    let size = 30;
-  for (let i = 1; i <= amount; i += 1){
-    const box = document.createElement('div');
-    box.style.margin = '10px';
+  let size = 30;
+  for (let i = 1; i <= amount; i++) {
+    const box = document.createElement("div");
+    box.style.margin = "10px";
     box.style.backgroundColor = getRandomHexColor();
-    boxesEl.appendChild(box);
-    box.style.width = size + 'px';
-    box.style.height = size + `px`;
+    box.style.width = size + "px";
+    box.style.height = size + "px";
     size += 10;
-    };
-};
-
-
-
-
+    refs.boxes.appendChild(box);
+  }
+}
